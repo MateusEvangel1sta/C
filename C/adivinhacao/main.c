10ed3c1ddc275bf21d8eb6538301a88e8229463d
Mateus Evangelista <enzoteuprogrammer@gmail.com>
2025-03-12 18:23:35 -0300

    Jogo Adivinhação - 02

--- C/adivinhacao/main.c
+++ C/adivinhacao/main.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
+#include <windows.h>
 
 int main() {
+  // Define a página de código do console para UTF-8
+  SetConsoleOutputCP(CP_UTF8);
+
   printf("************************************\n");
   printf("* Bem-vindo ao Jogo de Adivinhação *\n");
   printf("************************************\n");
@@ -10,7 +14,30 @@ int main() {
 
   int chute;
 
-  printf("Qual é o seu chute? ");
-  scanf("%d", &chute);
-  printf("Você chutou o número %d!", chute);
+  for (int i = 1; i <= 3; i++) {
+    printf("Qual é o seu chute? ");
+    scanf("%d", &chute);
+    printf("Seu chute foi %d\n", chute);
+  
+    int acertou = chute == numerosecreto;
+    
+    if (acertou) {
+      printf("Parabéns! Você acertou!\n");
+      break;
+    } else {
+      int maior = chute > numerosecreto;
+  
+      if (maior) {
+        printf("Seu chute foi maior que o número secreto!\n");
+      } else {
+        printf("Seu chute foi menor que o número secreto!\n");
+      }
+    }
+
+    printf("\n\n%d\n\n", i);
+  }
+
+  printf("Obrigado por jogar!");
+
+  return 0;
 }
